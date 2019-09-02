6ae1d83f0dcd4fdf3d5e89b2732402b4bcf2ea49
yuye@microsoft.com <yuye@microsoft.com>
2019-09-02 11:20:04 +0800

    update config

--- src/pylon/config/pylon.py
+++ src/pylon/config/pylon.py
@@ -41,7 +41,8 @@ class Pylon:
         if 'public-ip-address' in self.service_configuration:
             uri = 'http://{0}:{1}'.format(self.service_configuration['public-ip-address'], port)
             uriHttps = 'https://{0}'.format(self.service_configuration['public-ip-address'])
-
+        print(uri)
+        print(uriHttps)
         webhdfs_legacy_port = self.service_configuration['webhdfs-legacy-port']
         ret = {
             'port': port,
